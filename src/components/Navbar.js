f39ac3d6e48e291c0b67243ff5b1e50f6358b94a
Saiyam Vaid <saiyamvaid1997@gmail.com>
2023-01-16 15:47:02 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import finedine from "../assets/finedine.png";
 import "../App.css";
 import { useState } from "react";
 
@@ -11,7 +10,6 @@ function Navbar() {
     const [state4,setState4] = useState(true)
     const [state5,setState5] = useState(true)
     const [state6,setState6] = useState(true)
-    const [state7,setState7] = useState(true)
 
 const onMouseEnter1 = () => {
     setState1(false)
@@ -60,14 +58,6 @@ const onMouseEnter6 = () => {
 const onMouseLeave6 = () => {
   setState6(true)
 }
-
-const onMouseEnter7 = () => {
-  setState7(false)
-}
-
-const onMouseLeave7 = () => {
-  setState7(true)
-}
   return (
     <div>
 
@@ -81,14 +71,13 @@ const onMouseLeave7 = () => {
           marginRight: "7%"
         }}
       >
-        <img src={finedine} style={{ width: "140px" , height:"35px" , marginTop:"10px"}} />
+        <img src='https://media.licdn.com/dms/image/C4E1BAQGFEbmaEx6eSQ/company-background_10000/0/1519800837526?e=1674464400&v=beta&t=we5DM6FWVz5neWz-EwbclGdd5mdgQ8Rn6b24NtJQ18o' style={{ width: "140px" , height:"45px" , marginTop:"5px"}} />
         <p className="nav" style={{color:state1==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter1} onMouseLeave={onMouseLeave1}>Home</p>
         <p className="nav" style={{color:state2==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter2} onMouseLeave={onMouseLeave2}>About us</p>
         <p className="nav" style={{color:state3==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter3} onMouseLeave={onMouseLeave3}>Solutions</p>
-        <p className="nav" style={{color:state4==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter4} onMouseLeave={onMouseLeave4}>Blogs</p>
+        <p className="nav" style={{color:state4==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter4} onMouseLeave={onMouseLeave4}>Services</p>
         <p className="nav" style={{color:state5==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter5} onMouseLeave={onMouseLeave5}>Inquiry</p>
         <p className="nav" style={{color:state6==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter6} onMouseLeave={onMouseLeave6}>Contact us</p>
-        <p className="nav" style={{color:state7==true?'#5C6577':'#E6034B'}} onMouseEnter={onMouseEnter7} onMouseLeave={onMouseLeave7}>Services</p>
       </div> 
 
     </div>
